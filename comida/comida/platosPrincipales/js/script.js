3e07a92f388b256eb40ad6fef8b7071d318a7541
Nicolás <60797052+nicovicarg@users.noreply.github.com>
2022-09-27 07:29:33 -0300

    asd
    asd

---
+++ comida/comida/platosPrincipales/js/script.js
@@ -0,0 +1,17 @@
+let disparador = document.getElementById("checked");
+let resultado = 0;
+let sumatoria = 0;
+function comprobar(){
+  let contador = document.getElementById("contador");
+  let innerText = document.getElementById("contador");
+  contador.style.backgroundColor = "#00a900";
+  resultado += 1;
+  innerText.innerText = resultado;
+  sumatoria += 2660;
+  document.getElementById("sumatoria").innerText = `$ ${sumatoria}`;
+  
+}
+function sumar(){
+  document.getElementById
+}
+disparador.onclick = comprobar;
